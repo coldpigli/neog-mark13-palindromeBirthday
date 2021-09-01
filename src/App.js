de5a7821d7415e8aa1328828c5f133326dcf5ddf
coldpigli <piyushdas98@gmail.com>
2021-09-01 22:20:02 +0530

    UI completed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React, { useState } from 'react';
 function App() {
   
+  const[dateEntered, setDateEntered] = useState();
   
   return (
-  <h2>Initial project setup</h2>
+    <div className = "container">
+    <h1>Check if your Birth-date is a Palindrome or Not</h1>
+    <iframe className="gif" src="https://giphy.com/embed/bJO0LN9AMKtAbIDF34" width="200" height="200" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>
+    <label htmlFor="datePicker">Enter your Date-Of-Birth</label>
+    <input id = "datePicker" type = "date" onChange={(e)=>{
+      setDateEntered(e.target.value);
+    }}></input>
+    <button onClick = {()=>{console.log(dateEntered)}}>Check</button>
+  </div> 
     )
 }
 
